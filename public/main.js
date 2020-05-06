a9b148cfb88899fba590d5f3c46551989054016a
aiperieldar <aiperieldar.dev@gmail.com>
2020-05-05 21:31:45 -0400

    Initial Commit

--- public/main.js
+++ public/main.js
@@ -1,52 +1,61 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
 var trash = document.getElementsByClassName("fa-trash");
+var cross = document.getElementsByClassName("message")
+
 
 Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const date = this.parentNode.parentNode.childNodes[5].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
-        console.log(this.parentNode.parentNode.childNodes[1].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'date': date,
-            'thumbUp':thumbUp
-          })
+  element.addEventListener('click', function() {
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const date = this.parentNode.parentNode.childNodes[5].innerText
+    const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
+    console.log(this.parentNode.parentNode.childNodes[1].innerText)
+    fetch('messages', {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'name': name,
+          'msg': msg,
+          'date': date,
+          'thumbUp': thumbUp
         })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
+      })
+      .then(response => {
+        if (response.ok) return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        window.location.reload(true)
+      })
+  });
 });
 
 
 Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const date = this.parentNode.parentNode.childNodes[5].innerText
-        console.log(this.parentNode.parentNode.childNodes[1].innerText)
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'date': date
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
+  element.addEventListener('click', function() {
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const date = this.parentNode.parentNode.childNodes[5].innerText
+    console.log(this.parentNode.parentNode.childNodes[1].innerText)
+    fetch('messages', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'date': date
+      })
+    }).then(function(response) {
+      window.location.reload()
+    })
+  });
 });
+
+cross.addEventListener('dblclick', function (e) {
+  alert('done')
+  cross.classList.toggle('large');
+})
